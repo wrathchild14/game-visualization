cb877d080fcbc1760a2c17652b840dfc5b48805a
wrathchild14 <jovanprodanov123@gmail.com>
2022-10-27 23:14:33 +0200

    Add basic pie chart

--- VisualizationProject/sketch.js
+++ VisualizationProject/sketch.js
@@ -7,18 +7,39 @@ function preload() {
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    noStroke();
+    noLoop();
+}
+
+function draw() {
     background(200);
 
-    print(table);
+    
     let names = table.getColumn("Name");
     let platforms = table.getColumn("Platform");
     let releaseYear = table.getColumn("Year_of_Release");
     let globalSales = table.getColumn("Global_Sales");
     let developer = table.getColumn("Developer");
 
-    // Example of data
-    print(names[500], ", release year: " + releaseYear[500], " has global sales of " + globalSales[500]);
+    textSize(50);
+    text("Global sales", width/2 - 130, 100)
+
+    pieChart(800, globalSales.slice(0, 50));
 }
 
-function draw() {
+function pieChart(diameter, data) {
+    let lastAngle = 0;
+    for (let i = 0; i < data.length; i++) {
+        let gray = map(i, 0, data.length, 0, 255);
+        fill(gray);
+        arc(
+            width / 2,
+            height / 2,
+            diameter,
+            diameter,
+            lastAngle,
+            lastAngle + radians(data[i])
+        );
+        lastAngle += radians(data[i]);
+    }
 }
